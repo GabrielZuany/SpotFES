826785269c1d833e89f2a2de5732ebc39ed0b580
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-10-26 22:40:02 -0300

    Update tPropriedades.c

--- SourceCode/tPropriedades.c
+++ SourceCode/tPropriedades.c
@@ -1,5 +1,16 @@
 #include "tPropriedades.h"
 
 struct tPropriedades{
-
+    float danceability;
+    float energy;
+    int key;
+    float loudness;
+    int mode;
+    float speechiness;
+    float acousticness;
+    float instrumentalness;
+    float liveness;
+    float valence;
+    float tempo;
+    int time_signature;
 };
